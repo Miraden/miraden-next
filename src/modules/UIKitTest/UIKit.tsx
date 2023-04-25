8d3bc93820b97e374b066a809a0592022a5ef92d
Eugen Volodko <chocovelod@gmail.com>
2023-04-25 10:06:29 +0300

    feat: add button draft

--- src/modules/UIKitTest/UIKit.tsx
+++ src/modules/UIKitTest/UIKit.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui";
 import cn from "classnames";
 import styled from "styled-components";
 
@@ -9,10 +10,20 @@ const UIKit = ({ className }: UIKitProps) => {
   return (
     <StyledUIKit className={cn(className, "Container")}>
       <h1 className="Font_52_120">UI kit components</h1>
+      <div>
+        <Button>Button</Button>
+        <Button secondary className="UIKit__buttonSecondary">
+          Button
+        </Button>
+      </div>
     </StyledUIKit>
   );
 };
 
-const StyledUIKit = styled.section``;
+const StyledUIKit = styled.section`
+  .UIKit__buttonSecondary {
+    margin-top: 20px;
+  }
+`;
 
 export { UIKit };
