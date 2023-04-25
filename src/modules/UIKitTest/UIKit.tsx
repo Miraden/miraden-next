b70b6f80f4d128fd88a61cd7555b14febdac3424
Eugen Volodko <chocovelod@gmail.com>
2023-04-25 15:33:59 +0300

    fix: font fixes

--- src/modules/UIKitTest/UIKit.tsx
+++ src/modules/UIKitTest/UIKit.tsx
@@ -9,7 +9,7 @@ interface UIKitProps {
 const UIKit = ({ className }: UIKitProps) => {
   return (
     <StyledUIKit className={cn(className, "Container")}>
-      <h1 className="Font_52_120">UI kit components</h1>
+      <h1 className="Font_52_120">UI Kit</h1>
       <div className="UIKit__buttons">
         <Link type="button" href="/ui-kit/buttons">
           Buttons
