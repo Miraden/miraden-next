2d492a4c366858130df98b41cb35e0f1043df093
Eugen Volodko <chocovelod@gmail.com>
2023-05-02 16:16:25 +0300

    feat: reviews filter logic

--- src/modules/Base/Header/Header.tsx
+++ src/modules/Base/Header/Header.tsx
@@ -4,6 +4,7 @@ import { HeaderDesktop, HeaderMobile } from "./components";
 const Header = () => {
   return (
     <StyledHeader>
+      <div className="Header__topBlock" />
       <div className="Header">
         <HeaderDesktop className="Header__desktop" />
         <HeaderMobile className="Header__mobile" />
@@ -15,6 +16,14 @@ const Header = () => {
 const StyledHeader = styled.header`
   display: flex;
   justify-content: center;
+
+  .Header__topBlock {
+    z-index: 200;
+    position: fixed;
+    background: #eef1f5;
+    width: 100%;
+    height: 20px;
+  }
 
   .Header {
     position: fixed;
--- src/modules/Home/Reviews/Reviews.tsx
+++ src/modules/Home/Reviews/Reviews.tsx
@@ -1,6 +1,7 @@
+import { RequestButton } from "@/components/ui";
 import { SliderButton } from "@/icons";
 import { useKeenSlider } from "keen-slider/react";
-import { useCallback } from "react";
+import { SetStateAction, useCallback, useState } from "react";
 import styled from "styled-components";
 import { ReviewCard } from "./components/ReviewCard";
 
@@ -14,7 +15,7 @@ const reviews = [
   },
   {
     name: "Андрей Макеев",
-    role: "Клиент",
+    role: "Агенство",
     title: "Сопровождали во всем и разъясняли каждую деталь",
     text: "Хочу выразить благодарность Анастасии за её работу. Сотрудничать начали в середине февраля и так как квартиру искали по большому количеству критериев — закончили в марте. Понравилось больше всего, что сопровождали во всем, и разъясняли каждую деталь. Будем обращаться ещё!",
     image: "/images/avatar.jpg",
@@ -35,7 +36,7 @@ const reviews = [
   },
   {
     name: "Андрей Макеев",
-    role: "Клиент",
+    role: "Риелтор",
     title: "Сопровождали во всем и разъясняли каждую деталь",
     text: "Хочу выразить благодарность Анастасии за её работу. Сотрудничать начали в середине февраля и так как квартиру искали по большому количеству критериев — закончили в марте. Понравилось больше всего, что сопровождали во всем, и разъясняли каждую деталь. Будем обращаться ещё!",
     image: "/images/avatar.jpg",
@@ -49,7 +50,7 @@ const reviews = [
   },
   {
     name: "Андрей Макеев",
-    role: "Клиент",
+    role: "Застройщик",
     title: "Сопровождали во всем и разъясняли каждую деталь",
     text: "Хочу выразить благодарность Анастасии за её работу. Сотрудничать начали в середине февраля и так как квартиру искали по большому количеству критериев — закончили в марте. Понравилось больше всего, что сопровождали во всем, и разъясняли каждую деталь. Будем обращаться ещё!",
     image: "/images/avatar.jpg",
@@ -63,7 +64,7 @@ const reviews = [
   },
   {
     name: "Андрей Макеев",
-    role: "Клиент",
+    role: "Застройщик",
     title: "Сопровождали во всем и разъясняли каждую деталь",
     text: "Хочу выразить благодарность Анастасии за её работу. Сотрудничать начали в середине февраля и так как квартиру искали по большому количеству критериев — закончили в марте. Понравилось больше всего, что сопровождали во всем, и разъясняли каждую деталь. Будем обращаться ещё!",
     image: "/images/avatar.jpg",
@@ -88,24 +89,83 @@ const Reviews = () => {
     [instanceRef]
   );
 
+  const [role, setRole] = useState("Клиент");
+
+  const filteredRoles = reviews.filter((rev) =>
+    role ? rev.role.includes(role) : true
+  );
+
+  const handleButtonClick = (role: SetStateAction<string>) => {
+    setRole(role);
+  };
+
+  const isClient = reviews.some((rev) => rev.role.includes("Клиент"));
+  const isAgency = reviews.some((rev) => rev.role.includes("Агенство"));
+  const isRealtor = reviews.some((rev) => rev.role.includes("Риелтор"));
+  const isBuilders = reviews.some((rev) => rev.role.includes("Застройщик"));
+
+  const renderReviews = (revs: any[]) => {
+    return revs.map((rev, index) => (
+      <>
+        <div className="keen-slider__slide">
+          <ReviewCard
+            key={index}
+            text={rev.text}
+            title={rev.title}
+            role={rev.role}
+            name={rev.name}
+            image={rev.image}
+          />
+        </div>
+      </>
+    ));
+  };
+
   return (
     <StyledReviews>
       <div className="Container">
-        <h2>Нам доверяют</h2>
-        <div className="Reviews__slider keen-slider" ref={sliderRef}>
-          {reviews.map((review, index) => (
-            <div key={index} className="keen-slider__slide">
-              <ReviewCard
-                text={review.text}
-                title={review.title}
-                role={review.role}
-                name={review.name}
-                image={review.image}
-              />
-            </div>
-          ))}
-        </div>
+        <div className="Applications__head">
+          <h2 className="Font_44_120">Нам доверяют</h2>
+          <div className="Applications__tabs">
+            {isClient && (
+              <RequestButton
+                onClick={() => handleButtonClick("Клиент")}
+                active={role === "Клиент"}
+              >
+                Клиенты
+              </RequestButton>
+            )}
+            {isRealtor && (
+              <RequestButton
+                onClick={() => handleButtonClick("Риелтор")}
+                active={role === "Риелтор"}
+              >
+                Риелторы
+              </RequestButton>
+            )}
 
+            {isAgency && (
+              <RequestButton
+                onClick={() => handleButtonClick("Агентство")}
+                active={role === "Агентство"}
+              >
+                Агентства
+              </RequestButton>
+            )}
+
+            {isBuilders && (
+              <RequestButton
+                onClick={() => handleButtonClick("Застройщик")}
+                active={role === "Застройщик"}
+              >
+                Застройщики
+              </RequestButton>
+            )}
+          </div>
+        </div>
+        <div className="Reviews__slider keen-slider" ref={sliderRef}>
+          <>{renderReviews(filteredRoles)}</>
+        </div>
         <div className="Reviews__buttonsContainer">
           <button
             onClick={handleClickPrevButton}
@@ -124,9 +184,29 @@ const Reviews = () => {
 
 const StyledReviews = styled.section`
   overflow: hidden;
+  padding-top: 75px;
+  padding-bottom: 75px;
+
+  .Applications__head {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .Applications__tabs {
+    display: flex;
+    button:not(:first-child) {
+      margin-left: 10px;
+    }
+
+    button {
+      padding: 10px 20px;
+      width: fit-content;
+    }
+  }
 
   .Reviews__slider {
     display: flex;
+    margin-top: 50px;
   }
 
   .Reviews__buttonsContainer {
