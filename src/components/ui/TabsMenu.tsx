849008a926de608a05c038da5cdd2068c22d025e
Kirill Dyatlov <kirilldyatlovwebdev@gmail.com>
2023-06-25 05:19:52 +0300

    update navigation uikit

--- src/components/ui/TabsMenu.tsx
+++ src/components/ui/TabsMenu.tsx
@@ -8,11 +8,13 @@ class TabMenuItem {
   private readonly label: string;
   private readonly count: number;
   private readonly content: JSX.Element;
+  private readonly isDisabled: boolean;
 
-  constructor(label: string, count: number, content: JSX.Element) {
+  constructor(label: string, count: number, content: JSX.Element, isDisabled: boolean = false) {
     this.label = label;
     this.content = content;
     this.count = count;
+    this.isDisabled = isDisabled;
   }
 
   getContent(): JSX.Element {
@@ -27,6 +29,7 @@ class TabMenuItem {
         tertiary
         className={btnClassName}
         active={active}
+        disabled={this.isDisabled}
         attr={{'data-id': id}}
       >
         {this.label}
@@ -46,7 +49,7 @@ class TabsManager {
   private readonly items: Array<TabMenuItem>;
   public onClick: Function;
   private activeId: number;
-  private theme?: Themes;
+  private readonly theme?: Themes;
 
   constructor(callback: Function, theme: Themes = Themes.Light) {
     this.items = []
@@ -119,21 +122,32 @@ const MenuStyles = styled.div`
   margin-top: 30px;
 
   button.${baseClassName}__TabButton {
-    padding: 0;
+    padding: 2px;
     position: relative;
 
-    &:hover {
-      p {
-        color: ${({theme}) => theme.colors.blue.default} !important;
+    border-radius: 4px;
+    color: ${({theme}) => theme.colors.text.black};
+
+    &:focus-visible {
+      .TabsMenu__TabButtonCounter {
+        color: ${({theme}) => theme.colors.main};
       }
     }
 
-    &:focus {
+    &.active {
       background: transparent;
+      color: ${({theme}) => theme.colors.main};
+
+      .TabsMenu__TabButtonCounter {
+        color: ${({theme}) => theme.colors.main};
+      }
+
+      cursor: auto;
     }
 
-    &:not(:first-child) {
-      margin-left: 30px;
+    &:active {
+      background-color: ${({theme}) => theme.colors.white};
+      color: ${({theme}) => theme.colors.main};
     }
 
     &.active:before {
@@ -147,12 +161,72 @@ const MenuStyles = styled.div`
     }
   }
 
+  &.TabsMenu__menus--dark {
+    background: ${({theme}) => theme.colors.black};
+
+    .${baseClassName}__links-items {
+      .disabled {
+        color: ${({theme}) => theme.colors.background.grey};
+
+        .${baseClassName}__TabButtonCounter {
+          color: ${({theme}) => theme.colors.background.grey};
+        }
+
+        &:hover {
+          color: ${({theme}) => theme.colors.background.grey};
+        }
+      }
+    }
+
+    button.${baseClassName}__TabButton {
+      color: ${({theme}) => theme.colors.text.grey};
+      background: transparent;
+
+      &:hover {
+        color: ${({theme}) => theme.colors.text.grey};
+      }
+
+      &.active {
+        color: ${({theme}) => theme.colors.white};
+
+        .${baseClassName}__TabButtonCounter {
+          color: ${({theme}) => theme.colors.white};
+        }
+      }
+
+      &.active:before {
+        background: #FFF;
+      }
+
+      .${baseClassName}__TabButtonCounter {
+        color: ${({theme}) => theme.colors.text.grey};
+      }
+    }
+
+    .TabsMenu__links-divider {
+      background: transparent;
+    }
+  }
+
   .${baseClassName}__links-items {
     display: flex;
+    gap: 27px;
+
+    button.disabled {
+      background-color: transparent;
+
+      &:hover {
+        background-color: transparent;
+      }
+
+      .${baseClassName}__TabButtonCounter {
+        color: ${({theme}) => theme.colors.text.disabled};
+      }
+    }
   }
 
   .${baseClassName}__links-divider {
-    margin-top: 15px;
+    margin-top: 11px;
     width: 100%;
     background: ${({theme}) => theme.colors.grey.alt};
     height: 4px;
@@ -164,4 +238,4 @@ const MenuStyles = styled.div`
   }
 `
 
-export {TabsManager, TabMenuItem}
+export {TabsManager, TabMenuItem, Themes}
--- src/pages/ui-kit/navigation.tsx
+++ src/pages/ui-kit/navigation.tsx
@@ -1,7 +1,7 @@
 import {BlankLayout} from "@/modules/Base/BlankLayout";
 import UIKitHead from "@/modules/UIKitTest/UIKitHead";
 import styled from "styled-components";
-import {TabMenuItem, TabsManager} from "@/components/ui/TabsMenu";
+import {TabMenuItem, TabsManager, Themes} from "@/components/ui/TabsMenu";
 import React, {useCallback, useState} from "react";
 import {TabButtons} from "@/components/ui";
 
@@ -9,6 +9,11 @@ const NavigationsPage = () => {
   let [selected, setSelected] = useState<number>(0);
   const handleSelect = useCallback((state: number) => {
     setSelected(state);
+  }, []);
+
+  let [selectedDark, setSelectedDark] = useState<number>(0);
+  const handleSelectDark = useCallback((state: number) => {
+    setSelectedDark(state);
   }, []);
 
   return (
@@ -19,7 +24,8 @@ const NavigationsPage = () => {
         backUrl={"/ui-kit"}
       />
       <StyledNavigation className={"Container"}>
-        <div className={"NavSection"}>{renderClassic(selected, handleSelect)}</div>
+        <div
+          className={"NavSection"}>{renderClassic(selected, selectedDark, handleSelect, handleSelectDark)}</div>
         <TabButtons
           tabs={[
             {label: "Text", id: "1"},
@@ -32,17 +38,30 @@ const NavigationsPage = () => {
   )
 }
 
-function renderClassic(selected: number, handleSelect: Function): JSX.Element {
-  const tabsManager = new TabsManager(handleSelect)
-  tabsManager.addItem(new TabMenuItem('Все', 0, (<div>first</div>)))
-  tabsManager.addItem(new TabMenuItem('Опубликованные', 1, (<div>second</div>)))
-  tabsManager.addItem(new TabMenuItem('В архиве', 4, (<div>archived</div>)))
+function renderClassic(selected: number, selectedDark: number, handleSelect: Function, handleSelectDark: Function): JSX.Element {
+  const tabsManager = new TabsManager(handleSelect, Themes.Light)
+  tabsManager.addItem(new TabMenuItem('Tab classic', 0, (<div>first</div>)))
+  tabsManager.addItem(new TabMenuItem('Tab classic', 1, (<div>second</div>)))
+  tabsManager.addItem(new TabMenuItem('Tab classic', 4, (<div>archived</div>)))
+  tabsManager.addItem(new TabMenuItem('Tab classic', 4, (
+    <div>disabled</div>), true))
+
+  const tabsManagerDark = new TabsManager(handleSelectDark, Themes.Dark)
+  tabsManagerDark.addItem(new TabMenuItem('Tab classic', 1, (<div>first</div>)))
+  tabsManagerDark.addItem(new TabMenuItem('Tab classic', 1, (<div>second</div>)))
+  tabsManagerDark.addItem(new TabMenuItem('Tab classic', 1, (<div>disabled</div>), true))
 
   return (
     <>
       <h3 className={"Font_Accent_20_B NavSectionHeadline"}>{"Tab Classic"}</h3>
-      {tabsManager.renderMenus(selected)}
-      {tabsManager.renderContent(selected)}
+      <div>
+        {tabsManager.renderMenus(selected)}
+        {tabsManager.renderContent(selected)}
+      </div>
+      <div>
+        {tabsManagerDark.renderMenus(selectedDark)}
+        {tabsManagerDark.renderContent(selectedDark)}
+      </div>
     </>
   )
 }
@@ -56,6 +75,10 @@ const StyledNavigation = styled.div`
     text-transform: uppercase;
     color: ${({theme}) => theme.colors.main};
     margin-bottom: 20px;
+  }
+
+  .TabsMenu__menus--dark {
+    padding: 20px;
   }
 `
 
